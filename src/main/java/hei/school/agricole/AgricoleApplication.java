03a48a58cefb51ab0c5ae196c8822de07e5b9e4e
narindra20 <hei.narindra@gmail.com>
2026-04-17 11:51:24 +0300

    first commit

---
+++ src/main/java/hei/school/agricole/AgricoleApplication.java
@@ -0,0 +1,13 @@
+package hei.school.agricole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgricoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgricoleApplication.class, args);
+	}
+
+}
